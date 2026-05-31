14b1fff4e4d565de4f02c9e6d2777033614f8635
hugo ferey <ferey.hugo@gmail.com>
2026-05-31 18:34:39 +0200

    feat: finalisation de la page des transactions

--- src/components/Payout/payout.jsx
+++ src/components/Payout/payout.jsx
@@ -1,9 +1,37 @@
 import './payout.css'
+import {useState} from "react";
 
 function Payout(){
-    return (
-        <div className="payout"></div>
-    )
+    const [isOpen, setIsOpen] = useState(false);
+    return <>
+        <div>
+            <div className={`payout ${isOpen ? 'payout-open' : ''}`}>
+                <div className="pay-left">
+                    <p>27/02/20</p>
+                    <p>Golden Sun Bakery</p>
+                </div>
+                <div className="pay-right">
+                    <p>$8.00</p>
+                    <p>$298.00</p>
+                    <button className="info-btn" onClick={()=>setIsOpen(!isOpen)}>
+                        <p className={`info ${isOpen ? 'vOpen' : ''}`}>V</p>
+                    </button>
+                </div>
+            </div>
+            <div className={`info ${isOpen ? '' : 'infoOpen'}`}>
+                <div className="info-title">
+                    <p>Transaction type</p>
+                    <p> Category</p>
+                    <p> Note</p>
+                </div>
+                <div className="info-description">
+                    <p> Electronic</p>
+                    <p>Food <i className="fa-solid fa-pen pen"></i></p>
+                    <p>Lorem ispum <i className="fa-solid fa-pen pen"></i></p>
+                </div>
+            </div>
+        </div>
+    </>
 }
 
 export default Payout;
--- src/pages/Transaction/transaction.jsx
+++ src/pages/Transaction/transaction.jsx
@@ -1,15 +1,34 @@
 import './transaction.css'
 import Account from "../../components/Account/Account.jsx";
+import Payout from "../../components/Payout/payout.jsx";
 
 function Transaction() {
-    return (
-        <Account
-            transactionMode="true"
-            title="Argent Bank Checking (x8349)"
-            amount="2,082.79"
-            desc="Available Balance"
-        />
-    )
+    return<>
+        <main className="main page">
+            <Account
+                transactionMode="true"
+                title="Argent Bank Checking (x8349)"
+                amount="2,082.79"
+                desc="Available Balance"
+            />
+            <div className="content">
+                <div className="content-left">
+                    <p>Date</p>
+                    <p>Description</p>
+                </div>
+                <div className="content-right">
+                    <p>Amount</p>
+                    <p className="balance">Balance</p>
+                </div>
+            </div>
+            <section className="payment">
+                {Array.from({length: 5}).map((_, i) =>(
+                    <Payout key={i}/>
+                ))}
+        </section>
+        </main>
+
+    </>
 }
 
 export default Transaction;
